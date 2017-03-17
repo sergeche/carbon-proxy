26bac8c8740b7de65546acab928839fba1f9f083
Sergey Chikuyonok <serge.che@gmail.com>
2017-03-17 11:38:59 +0300

    Support CLI options

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const programm = require('commander');
 const connect = require('connect');
 const counter = require('./lib/counter');
 const proxy = require('./lib/proxy');
 
-const port = 8081;
-const proxyHost = 'sup.emmet.io';
+programm
+.version(require('./package.json').version)
+.option('-p, --port <n>', 'Server port', parseInt, 8081)
+.option('-h, --host <s>', 'Proxy host', 'localhost')
+.parse(process.argv);
 
 /**
  * Rewrites value of ads JSON
@@ -14,13 +18,13 @@ const proxyHost = 'sup.emmet.io';
  * @return {String}       New value for `key`
  */
 function rewrite(key, value) {
-	return `//${proxyHost}/${key === 'fetch' ? 'f' : 'p'}/${proxy.encodeUrl(value)}`;
+	return `//${programm.host}/${key === 'fetch' ? 'f' : 'p'}/${proxy.encodeUrl(value)}`;
 }
 
 connect()
 .use('/ads', counter(req => `http://srv.carbonads.net${req.originalUrl}`, rewrite))
 .use('/f', counter(req => proxy.decodeUrl(req.url), rewrite))
 .use('/p', proxy())
-.listen(port);
+.listen(programm.port);
 
-console.log('Started app http://localhost:', port);
+console.log('Started app http://localhost:%d', programm.port);
