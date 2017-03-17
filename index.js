a275f3675f5d3d3551163b96720f3c7ce178d3c3
Sergey Chikuyonok <serge.che@gmail.com>
2017-03-17 11:27:50 +0300

    Properly update `fetch` ads code branch

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const counter = require('./lib/counter');
 const proxy = require('./lib/proxy');
 
 const port = 8081;
+const proxyHost = 'sup.emmet.io';
+
+/**
+ * Rewrites value of ads JSON
+ * @param  {String} key   JSON key
+ * @param  {String} value JSON value
+ * @return {String}       New value for `key`
+ */
+function rewrite(key, value) {
+	return `//${proxyHost}/${key === 'fetch' ? 'f' : 'p'}/${proxy.encodeUrl(value)}`;
+}
 
 connect()
-.use('/ads', counter('srv.carbonads.net', 'sup.emmet.io'))
+.use('/ads', counter(req => `http://srv.carbonads.net${req.originalUrl}`, rewrite))
+.use('/f', counter(req => proxy.decodeUrl(req.url), rewrite))
 .use('/p', proxy())
 .listen(port);
 
